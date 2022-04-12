bdca15217229653b8f9a1121c55559ba2a2de1fc
jules <101470183+julesdellares@users.noreply.github.com>
2022-04-11 22:58:02 -0700

    Update notesAPI.js
    more leanring about JSON. didn't get as far as expected tongiht but oh well!

--- notesAPI.js
+++ notesAPI.js
@@ -1,29 +1,45 @@
-// interacts with local storage to retrieve/save/delete notes
+//interacts with local storage for notes
 
 export default class NotesAPI {
+
     static getAllNotes() {
-        // if there are no existing notes it will run and give us an empty array
-       const notes = JSON.parse(localStorage.getItem("notesapp-notes" || "[]"))
-        //this is a sort algorithim - look into these more!
+        const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+//sorts notes by timestamp
         return notes.sort((a, b) => {
             return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
         });
+
     }
 
     static saveNotes(noteToSave) {
         const notes = NotesAPI.getAllNotes();
+        const existing = notes.find(note => note.id => noteToSave.id);
 
-        noteToSave.id = Math.floor(Math.random() * 1000000);
-        noteToSave.updated = new Date().toISOString();
-        notes.push(noteToSave);
+        //edit update note
+
+        if (existing) {
+            existing.title = noteToSave.title;
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toDateString();
+
+
+        } else {
+            noteToSave.id = Math.floor(Math.random() * 100000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
+
+
 
         localStorage.setItem("notesapp-notes", JSON.stringify(notes));
-
 
     }
 
     static deleteNote(id) {
+        const notes = NotesAPI.getAllNotes();
+        const newNotes = notes.filter(note => note.id != id);
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
 
     }
-
 }
