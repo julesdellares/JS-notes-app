e9a182469664e12df95fef5f6fab1825fc397ece
jules <101470183+julesdellares@users.noreply.github.com>
2022-04-13 23:30:25 -0700

    Update main.js
    not much today, but i didn't want to leave today at 0

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ import NotesView from "./NotesView.js"
 
 const app = document.getElementById("app");
 const view = new NotesView(app, {
-    onNoteSelect() {
-        console.log("Add a new note");
-    }
+    onNoteAdd() {
+        console.log("Add A New Note");
+    },
+    onNoteEdit(newTitle, newBody) {
+        console.log(newTitle);
+        console.log(newBody)
+    },
+
 });
